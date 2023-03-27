dbc5b8dfa8e3561fbb3eacf7aa0dd8da7b284464
Endenisance <dov.raven@gmail.com>
2023-03-28 00:49:28 +0300

    added full base script of the game

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -2,11 +2,25 @@ import random
 
 
 def is_vaild(number):
-    return 1 <= number <=100
-
+    return 1 <= number <= 100
 
 
 random_number = random.randint(1, 100)
 print('Добро пожаловать в числовую угадайку')
 
-print('bla bla bla')
+print('Попробуйте отгадать, какое целое число загадал компьютерjn 1 до 100?')
+while True:
+    num = int(input())
+    if is_vaild(num) == False:
+        print('А может быть все-таки введем целое число от 1 до 100?')
+        continue
+    if num > random_number:
+        print('Ваше число больше загаданного, попробуйте еще разок')
+        continue
+    if num < random_number:
+        print('Ваше число меньше загаданного, попробуйте еще разок')
+        continue
+    if num == random_number:
+        print('Вы угадали, поздравляем!')
+        break
+print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
